c653ec55143198c501b30070ae7e6f112936d892
Artem Abeleshev <abeleshev@gmail.com>
2022-09-27 18:49:42 +0300

    Update entity related base classes to add serialization support (#8)

--- aes-core/src/main/java/com/ijioio/aes/core/BaseEntity.java
+++ aes-core/src/main/java/com/ijioio/aes/core/BaseEntity.java
@@ -1,12 +1,40 @@
 package com.ijioio.aes.core;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.ijioio.aes.core.serialization.SerializationContext;
+import com.ijioio.aes.core.serialization.SerializationHandler;
+import com.ijioio.aes.core.serialization.SerializationReader;
+import com.ijioio.aes.core.serialization.SerializationWriter;
+
 /**
  * Base class for all entities.
  */
 public abstract class BaseEntity extends BaseIdentity implements Entity {
 
-  @Override
-  public String toString() {
-    return "BaseEntity [id=" + getId() + ", type=" + getClass().getName() + "]";
-  }
+	@Override
+	public Map<String, SerializationWriter> getWriters(SerializationContext context, SerializationHandler handler) {
+
+		Map<String, SerializationWriter> writers = new LinkedHashMap<>();
+
+		writers.put("id", () -> handler.write(context, "id", getId()));
+
+		return writers;
+	}
+
+	@Override
+	public Map<String, SerializationReader> getReaders(SerializationContext context, SerializationHandler handler) {
+
+		Map<String, SerializationReader> readers = new LinkedHashMap<>();
+
+		readers.put("id", () -> setId(handler.read(context, getId())));
+
+		return readers;
+	}
+
+	@Override
+	public String toString() {
+		return "BaseEntity [id=" + getId() + ", type=" + getClass().getName() + "]";
+	}
 }
--- aes-core/src/main/java/com/ijioio/aes/core/EntityContainer.java
+++ aes-core/src/main/java/com/ijioio/aes/core/EntityContainer.java
@@ -1,6 +1,12 @@
 package com.ijioio.aes.core;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+
+import com.ijioio.aes.core.serialization.SerializationContext;
+import com.ijioio.aes.core.serialization.SerializationHandler;
+import com.ijioio.aes.core.serialization.SerializationReader;
+import com.ijioio.aes.core.serialization.SerializationWriter;
 
 /**
  * Convenient class for holding entity.
@@ -10,8 +16,7 @@ public class EntityContainer<E extends Entity> extends BaseEntity {
 	/**
 	 * Creates entity container holding indicated {@code entity}.
 	 *
-	 * @param entity
-	 *            to be place to the container
+	 * @param entity to be place to the container
 	 * @return entity container holding entity
 	 */
 	@SuppressWarnings("unchecked")
@@ -70,9 +75,18 @@ public class EntityContainer<E extends Entity> extends BaseEntity {
 	}
 
 	@Override
+	public Map<String, SerializationWriter> getWriters(SerializationContext context, SerializationHandler handler) {
+		throw new RuntimeException("not implemented");
+	}
+
+	@Override
+	public Map<String, SerializationReader> getReaders(SerializationContext context, SerializationHandler handler) {
+		throw new RuntimeException("not implemented");
+	}
+
+	@Override
 	public String toString() {
-		return "EntityContainer [id=" + getId() + ", entityType=" + entityType
-				+ ", entity=" + entity + ", createdAt=" + createdAt
-				+ ", modifiedAt=" + modifiedAt + "]";
+		return "EntityContainer [id=" + getId() + ", entityType=" + entityType + ", entity=" + entity + ", createdAt="
+				+ createdAt + ", modifiedAt=" + modifiedAt + "]";
 	}
 }
