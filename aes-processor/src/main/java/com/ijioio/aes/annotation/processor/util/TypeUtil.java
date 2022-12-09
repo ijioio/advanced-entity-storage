6554af0092fc19b85827d73ecab0050ff2720fe0
Artem Abeleshev <abeleshev@gmail.com>
2022-12-09 22:38:53 +0300

    Update type util to use type handlers (#53)

--- aes-processor/src/main/java/com/ijioio/aes/annotation/processor/util/TypeUtil.java
+++ aes-processor/src/main/java/com/ijioio/aes/annotation/processor/util/TypeUtil.java
@@ -1,7 +1,103 @@
 package com.ijioio.aes.annotation.processor.util;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import com.ijioio.aes.annotation.Type;
+
 /** Helper class for type. */
 public class TypeUtil {
+
+	/**
+	 * Fully qualified name for boolean type.
+	 */
+	public static final String BOOLEAN_TYPE_NAME = boolean.class.getName();
+
+	/**
+	 * Fully qualified name for char type.
+	 */
+	public static final String CHAR_TYPE_NAME = char.class.getName();
+
+	/**
+	 * Fully qualified name for byte type.
+	 */
+	public static final String BYTE_TYPE_NAME = byte.class.getName();
+
+	/**
+	 * Fully qualified name for short type.
+	 */
+	public static final String SHORT_TYPE_NAME = short.class.getName();
+
+	/**
+	 * Fully qualified name for int type.
+	 */
+	public static final String INT_TYPE_NAME = int.class.getName();
+
+	/**
+	 * Fully qualified name for long type.
+	 */
+	public static final String LONG_TYPE_NAME = long.class.getName();
+
+	/**
+	 * Fully qualified name for float type.
+	 */
+	public static final String FLOAT_TYPE_NAME = float.class.getName();
+
+	/**
+	 * Fully qualified name for double type.
+	 */
+	public static final String DOUBLE_TYPE_NAME = double.class.getName();
+
+	/**
+	 * Fully qualified name for byte array type.
+	 */
+	public static final String BYTE_ARRAY_TYPE_NAME = byte[].class.getName();
+
+	/**
+	 * Fully qualified name for {@link String} type.
+	 */
+	public static final String STRING_TYPE_NAME = String.class.getName();
+
+	/**
+	 * Fully qualified name for {@link Instant} type.
+	 */
+	public static final String INSTANT_TYPE_NAME = Instant.class.getName();
+
+	/**
+	 * Fully qualified name for {@link LocalDateTime} type.
+	 */
+	public static final String LOCAL_DATE_TIME_TYPE_NAME = LocalDateTime.class.getName();
+
+	/**
+	 * Fully qualified name for {@link LocalDate} type.
+	 */
+	public static final String LOCAL_DATE_TYPE_NAME = LocalDate.class.getName();
+
+	/**
+	 * Fully qualified name for {@link LocalTime} type.
+	 */
+	public static final String LOCAL_TIME_TYPE_NAME = LocalTime.class.getName();
+
+	/**
+	 * Fully qualified name for {@link List} type.
+	 */
+	public static final String LIST_TYPE_NAME = List.class.getName();
+
+	/**
+	 * Fully qualified name for {@link Set} type.
+	 */
+	public static final String SET_TYPE_NAME = Set.class.getName();
+
+	/**
+	 * Fully qualified name for {@link Map} type.
+	 */
+	public static final String MAP_TYPE_NAME = Map.class.getName();
 
 	/**
 	 * Fully qualified name for base entity type.
@@ -37,4 +133,405 @@ public class TypeUtil {
 	 * Fully qualified name for serialization reader.
 	 */
 	public static final String SERIALIZATION_READER_TYPE_NAME = "com.ijioio.aes.core.serialization.SerializationReader";
+
+	private static final Pattern pattern = Pattern.compile("\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*"
+			+ "(\\.\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*)*");
+
+	/**
+	 * Type handler providing some various actions.
+	 */
+	public static interface TypeHandler {
+
+		/**
+		 * Defines if type is immutable.
+		 * 
+		 * @return {@code true} if type is immutable, {@code false} otherwise
+		 */
+		public boolean isImmutable();
+
+		/**
+		 * Defines if type is a valid identifier.
+		 * 
+		 * @return {@code true} if type is valid identifier, {@code false} otherwise
+		 */
+		public boolean isValidIdentifier();
+	}
+
+	/**
+	 * Type handler of boolean type.
+	 */
+	public static final TypeHandler BOOLEAN_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of char type.
+	 */
+	public static final TypeHandler CHAR_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of byte type.
+	 */
+	public static final TypeHandler BYTE_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of short type.
+	 */
+	public static final TypeHandler SHORT_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of int type.
+	 */
+	public static final TypeHandler INT_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of long type.
+	 */
+	public static final TypeHandler LONG_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of float type.
+	 */
+	public static final TypeHandler FLOAT_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of double type.
+	 */
+	public static final TypeHandler DOUBLE_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of byte array type.
+	 */
+	public static final TypeHandler BYTE_ARRAY_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link String} type.
+	 */
+	public static final TypeHandler STRING_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link Instant} type.
+	 */
+	public static final TypeHandler INSTANT_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link LocalDateTime} type.
+	 */
+	public static final TypeHandler LOCAL_DATE_TIME_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link LocalDate} type.
+	 */
+	public static final TypeHandler LOCAL_DATE_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link LocalTime} type.
+	 */
+	public static final TypeHandler LOCAL_TIME_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return true;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link List} type.
+	 */
+	public static final TypeHandler LIST_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return false;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link Set} type.
+	 */
+	public static final TypeHandler SET_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return false;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Type handler of {@link Map} type.
+	 */
+	public static final TypeHandler MAP_TYPE_HANDLER = new TypeHandler() {
+
+		@Override
+		public boolean isImmutable() {
+			return false;
+		}
+
+		@Override
+		public boolean isValidIdentifier() {
+			return true;
+		}
+	};
+
+	/**
+	 * Defines if type with indicated {@code name} is immutable. The {@code name}
+	 * can be either type alias or type fully qualified name.
+	 * 
+	 * @param name of the type
+	 * @return {@code true} if type is immutable, {@code false} otherwise
+	 */
+	public static boolean isImmutable(String name) {
+		return getTypeHandler(name).isImmutable();
+	}
+
+	/**
+	 * Defines if type with indicated {@code name} is valid identifier. The
+	 * {@code name} can be either type alias or type fully qualified name.
+	 * 
+	 * @param name of the type
+	 * @return {@code true} if type is valid identifier, {@code false} otherwise
+	 */
+	public static boolean isValidIdentifier(String name) {
+		return getTypeHandler(name).isValidIdentifier();
+	}
+
+	private static TypeHandler getTypeHandler(String name) {
+
+		if (name.equals(Type.BOOLEAN) || name.equals(BOOLEAN_TYPE_NAME)) {
+			return BOOLEAN_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.CHAR) || name.equals(CHAR_TYPE_NAME)) {
+			return CHAR_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.BYTE) || name.equals(BYTE_TYPE_NAME)) {
+			return BYTE_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.SHORT) || name.equals(SHORT_TYPE_NAME)) {
+			return SHORT_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.INT) || name.equals(INT_TYPE_NAME)) {
+			return INT_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.LONG) || name.equals(LONG_TYPE_NAME)) {
+			return LONG_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.FLOAT) || name.equals(FLOAT_TYPE_NAME)) {
+			return FLOAT_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.DOUBLE) || name.equals(DOUBLE_TYPE_NAME)) {
+			return DOUBLE_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.BYTE_ARRAY) || name.equals(BYTE_ARRAY_TYPE_NAME)) {
+			return BYTE_ARRAY_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.STRING) || name.equals(STRING_TYPE_NAME)) {
+			return STRING_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.INSTANT) || name.equals(INSTANT_TYPE_NAME)) {
+			return INSTANT_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.LOCAL_DATE_TIME) || name.equals(LOCAL_DATE_TIME_TYPE_NAME)) {
+			return LOCAL_DATE_TIME_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.LOCAL_DATE) || name.equals(LOCAL_DATE_TYPE_NAME)) {
+			return LOCAL_DATE_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.LOCAL_TIME) || name.equals(LOCAL_TIME_TYPE_NAME)) {
+			return LOCAL_TIME_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.LIST) || name.equals(LIST_TYPE_NAME)) {
+			return LIST_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.SET) || name.equals(SET_TYPE_NAME)) {
+			return SET_TYPE_HANDLER;
+		}
+
+		if (name.equals(Type.MAP) || name.equals(MAP_TYPE_NAME)) {
+			return MAP_TYPE_HANDLER;
+		}
+
+		return new TypeHandler() {
+
+			@Override
+			public boolean isImmutable() {
+				return false;
+			}
+
+			@Override
+			public boolean isValidIdentifier() {
+				return pattern.matcher(name).matches();
+			}
+		};
+	}
 }
