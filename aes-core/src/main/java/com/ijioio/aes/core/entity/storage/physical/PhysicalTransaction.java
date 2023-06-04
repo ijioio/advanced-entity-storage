4706396174a9505fc33eca111b26fa3c5ae6735c
abeleshev <abeleshev@gmail.com>
2023-06-04 14:16:29 +0900

    Update physical transaction interface to include begin method (#77)

--- aes-core/src/main/java/com/ijioio/aes/core/entity/storage/physical/PhysicalTransaction.java
+++ aes-core/src/main/java/com/ijioio/aes/core/entity/storage/physical/PhysicalTransaction.java
@@ -4,6 +4,8 @@ import com.ijioio.aes.core.entity.storage.StorageException;
 
 public interface PhysicalTransaction {
 
+	public void begin() throws StorageException;
+
 	public void commit() throws StorageException;
 
 	public void rollback() throws StorageException;
