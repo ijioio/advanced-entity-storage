b54d459613b58b7ca3df69f73c7a5a3446620555
abeleshev <abeleshev@gmail.com>
2023-06-20 23:06:20 +0900

    Update base entity index to base implement inherited methods (#84)

--- aes-core/src/main/java/com/ijioio/aes/core/BaseEntityIndex.java
+++ aes-core/src/main/java/com/ijioio/aes/core/BaseEntityIndex.java
@@ -1,23 +1,40 @@
 package com.ijioio.aes.core;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ijioio.aes.core.persistence.PersistenceColumnProvider;
+import com.ijioio.aes.core.persistence.PersistenceContext;
+import com.ijioio.aes.core.persistence.PersistenceException;
+import com.ijioio.aes.core.persistence.PersistenceHandler;
+import com.ijioio.aes.core.persistence.PersistenceReader;
+import com.ijioio.aes.core.persistence.PersistenceWriter;
+
 /**
  * Base class for all entity indexes.
  */
-public abstract class BaseEntityIndex<E extends Entity> implements EntityIndex<E> {
+public abstract class BaseEntityIndex<E extends Entity> extends BaseIdentity implements EntityIndex<E> {
 
-	private String id;
+	public static class Properties {
+
+		public static final Property<String> id = Property.of("id", String.class);
+
+		@SuppressWarnings("rawtypes")
+		public static final Property<EntityReference> source = Property.of("source", EntityReference.class);
+
+		private static final List<Property<?>> values = new ArrayList<>();
+
+		static {
+
+			values.add(id);
+			values.add(source);
+		}
+	}
 
 	private EntityReference<E> source;
-
-	@Override
-	public String getId() {
-		return id;
-	}
-
-	@Override
-	public void setId(String id) {
-		this.id = id;
-	}
 
 	@Override
 	public EntityReference<E> getSource() {
@@ -26,5 +43,47 @@ public abstract class BaseEntityIndex<E extends Entity> implements EntityIndex<E
 
 	public void setSource(EntityReference<E> source) {
 		this.source = source;
+	}
+
+	public Collection<Property<?>> getProperties() {
+		return Properties.values;
+	}
+
+	@Override
+	public void insert(PersistenceContext context, PersistenceHandler handler) throws PersistenceException {
+		handler.insert(context, getClass().getSimpleName(), getProperties(), getColumnProviders(context, handler),
+				getWriters(context, handler));
+	}
+
+	public Map<String, PersistenceColumnProvider> getColumnProviders(PersistenceContext context,
+			PersistenceHandler handler) {
+
+		Map<String, PersistenceColumnProvider> columnProviders = new LinkedHashMap<>();
+
+		columnProviders.put("id", () -> handler.getColumns(context, "id", String.class));
+		columnProviders.put("source", () -> handler.getColumns(context, "source", EntityReference.class));
+
+		return columnProviders;
+	}
+
+	public Map<String, PersistenceWriter> getWriters(PersistenceContext context, PersistenceHandler handler) {
+
+		Map<String, PersistenceWriter> writers = new LinkedHashMap<>();
+
+		writers.put("id", () -> handler.write(context, "id", getId(), String.class));
+		writers.put("source", () -> handler.write(context, "source", getSource(), EntityReference.class));
+
+		return writers;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, PersistenceReader> getReaders(PersistenceContext context, PersistenceHandler handler) {
+
+		Map<String, PersistenceReader> readers = new LinkedHashMap<>();
+
+		readers.put("id", () -> setId(handler.read(context, "id", getId(), String.class)));
+		readers.put("source", () -> setSource(handler.read(context, "source", getSource(), EntityReference.class)));
+
+		return readers;
 	}
 }
