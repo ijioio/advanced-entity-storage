e074bff813f3cd95ff3db9d071c7cdf23bf8c0f6
abeleshev <abeleshev@gmail.com>
2023-07-17 15:43:34 +0900

    Add jdbc persistence context (#84)

---
+++ aes-core/src/main/java/com/ijioio/aes/core/persistence/jdbc/JdbcPersistenceContext.java
@@ -0,0 +1,50 @@
+package com.ijioio.aes.core.persistence.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.ijioio.aes.core.persistence.PersistenceContext;
+
+public class JdbcPersistenceContext implements PersistenceContext {
+
+	public static JdbcPersistenceContext of(Connection connection) {
+		return new JdbcPersistenceContext(connection);
+	}
+
+	private final Connection connection;
+
+	private PreparedStatement statement;
+
+	private ResultSet resultSet;
+
+	private int index = 0;
+
+	private JdbcPersistenceContext(Connection connection) {
+		this.connection = connection;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public PreparedStatement getStatement() {
+		return statement;
+	}
+
+	public void setStatement(PreparedStatement statement) {
+		this.statement = statement;
+	}
+
+	public ResultSet getResultSet() {
+		return resultSet;
+	}
+
+	public void setResultSet(ResultSet resultSet) {
+		this.resultSet = resultSet;
+	}
+
+	public int getNextIndex() {
+		return index++;
+	}
+}
