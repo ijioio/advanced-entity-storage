0db02f174546948fa19ce2228c63fd71c832556f
abeleshev <abeleshev@gmail.com>
2023-07-30 17:36:54 +0900

    Update entity index to use readable and writable interfaces (#84)

--- aes-core/src/main/java/com/ijioio/aes/core/BaseEntityIndex.java
+++ aes-core/src/main/java/com/ijioio/aes/core/BaseEntityIndex.java
@@ -6,13 +6,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.ijioio.aes.core.persistence.PersistenceColumnProvider;
-import com.ijioio.aes.core.persistence.PersistenceContext;
-import com.ijioio.aes.core.persistence.PersistenceException;
-import com.ijioio.aes.core.persistence.PersistenceHandler;
-import com.ijioio.aes.core.persistence.PersistenceReader;
-import com.ijioio.aes.core.persistence.PersistenceWriter;
-
 /**
  * Base class for all entity indexes.
  */
@@ -22,8 +15,7 @@ public abstract class BaseEntityIndex<E extends Entity> extends BaseIdentity imp
 
 		public static final Property<String> id = Property.of("id", String.class);
 
-		@SuppressWarnings("rawtypes")
-		public static final Property<EntityReference> source = Property.of("source", EntityReference.class);
+		public static final Property<EntityReference<?>> source = Property.of("source", EntityReference.class);
 
 		private static final List<Property<?>> values = new ArrayList<>();
 
@@ -45,61 +37,29 @@ public abstract class BaseEntityIndex<E extends Entity> extends BaseIdentity imp
 		this.source = source;
 	}
 
+	@Override
 	public Collection<Property<?>> getProperties() {
 		return Properties.values;
 	}
 
 	@Override
-	public void read(PersistenceHandler handler, PersistenceContext context) throws PersistenceException {
-		handler.read(context, getProperties(), getReaders(handler, context));
-	}
+	public Map<Property<?>, PropertyWriter<?>> getWriters() {
 
-	@Override
-	public void create(PersistenceHandler handler, PersistenceContext context) throws PersistenceException {
-		handler.create(context, getClass().getSimpleName(), getProperties(), getColumnProviders(handler, context),
-				getWriters(handler, context));
-	}
+		Map<Property<?>, PropertyWriter<?>> writers = new LinkedHashMap<>();
 
-	@Override
-	public void update(PersistenceHandler handler, PersistenceContext context) throws PersistenceException {
-		handler.update(context, getClass().getSimpleName(), Properties.id, getProperties(),
-				getColumnProviders(handler, context), getWriters(handler, context));
-	}
-
-	@Override
-	public void delete(PersistenceHandler handler, PersistenceContext context) throws PersistenceException {
-		handler.delete(context, getClass().getSimpleName(), Properties.id, getColumnProviders(handler, context),
-				getWriters(handler, context));
-	}
-
-	public Map<String, PersistenceColumnProvider> getColumnProviders(PersistenceHandler handler,
-			PersistenceContext context) {
-
-		Map<String, PersistenceColumnProvider> columnProviders = new LinkedHashMap<>();
-
-		columnProviders.put("id", () -> handler.getColumns(context, "id", String.class));
-		columnProviders.put("source", () -> handler.getColumns(context, "source", EntityReference.class));
-
-		return columnProviders;
-	}
-
-	public Map<String, PersistenceWriter> getWriters(PersistenceHandler handler, PersistenceContext context) {
-
-		Map<String, PersistenceWriter> writers = new LinkedHashMap<>();
-
-		writers.put("id", () -> handler.write(context, "id", getId(), String.class));
-		writers.put("source", () -> handler.write(context, "source", getSource(), EntityReference.class));
+		writers.put(Properties.id, (String value) -> setId(value));
+		writers.put(Properties.source, (EntityReference<E> value) -> setSource(value));
 
 		return writers;
 	}
 
-	@SuppressWarnings("unchecked")
-	public Map<String, PersistenceReader> getReaders(PersistenceHandler handler, PersistenceContext context) {
+	@Override
+	public Map<Property<?>, PropertyReader<?>> getReaders() {
 
-		Map<String, PersistenceReader> readers = new LinkedHashMap<>();
+		Map<Property<?>, PropertyReader<?>> readers = new LinkedHashMap<>();
 
-		readers.put("id", () -> setId(handler.read(context, "id", getId(), String.class)));
-		readers.put("source", () -> setSource(handler.read(context, "source", getSource(), EntityReference.class)));
+		readers.put(Properties.id, () -> getId());
+		readers.put(Properties.source, () -> getSource());
 
 		return readers;
 	}
