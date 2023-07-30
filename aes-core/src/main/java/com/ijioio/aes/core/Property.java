3f5be09da2eae80bc51d5c3a9146adef70fa2231
abeleshev <abeleshev@gmail.com>
2023-07-30 17:20:47 +0900

    Update property to use full type generics (#84)

--- aes-core/src/main/java/com/ijioio/aes/core/Property.java
+++ aes-core/src/main/java/com/ijioio/aes/core/Property.java
@@ -1,9 +1,12 @@
 package com.ijioio.aes.core;
 
+import java.util.Objects;
+
 public class Property<T> {
 
-	public static <T> Property<T> of(String name, Class<T> type) {
-		return new Property<>(name, type);
+	@SuppressWarnings("unchecked")
+	public static <T, A extends T> Property<A> of(String name, Class<T> type) {
+		return (Property<A>) new Property<>(name, type);
 	}
 
 	private final String name;
@@ -22,6 +25,31 @@ public class Property<T> {
 
 	public Class<T> getType() {
 		return type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj == null) {
+			return false;
+		}
+
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		Property<?> other = (Property<?>) obj;
+
+		return Objects.equals(name, other.name) && Objects.equals(type, other.type);
 	}
 
 	@Override
