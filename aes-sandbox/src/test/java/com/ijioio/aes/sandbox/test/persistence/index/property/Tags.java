3678b60527bbf26e59a5f76f3f2dc2a403711d05
abeleshev <abeleshev@gmail.com>
2023-10-17 16:55:45 +0900

    Add tags constants (#114)

---
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/persistence/index/property/Tags.java
@@ -0,0 +1,10 @@
+package com.ijioio.aes.sandbox.test.persistence.index.property;
+
+public class Tags {
+
+	public static final String REGULAR = "regular";
+
+	public static final String EMPTY = "empty";
+
+	public static final String NULL = "null";
+}
