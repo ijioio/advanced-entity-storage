5c2b75f40332f67e9033db921923ea15b18b5ebf
abeleshev <abeleshev@gmail.com>
2023-10-20 18:30:44 +0900

    Add property list final delete persistence tests (#114)

---
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/persistence/index/property/PropertyEntityReferenceListFinalDeletePersistenceTest.java
@@ -0,0 +1,193 @@
+package com.ijioio.aes.sandbox.test.persistence.index.property;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Assertions;
+
+import com.ijioio.aes.annotation.Attribute;
+import com.ijioio.aes.annotation.Entity;
+import com.ijioio.aes.annotation.EntityIndex;
+import com.ijioio.aes.annotation.EntityIndexProperty;
+import com.ijioio.aes.annotation.EntityProperty;
+import com.ijioio.aes.annotation.Parameter;
+import com.ijioio.aes.annotation.Type;
+import com.ijioio.aes.core.EntityReference;
+import com.ijioio.aes.core.Property;
+import com.ijioio.aes.sandbox.test.persistence.index.property.BasePropertyDeletePersistenceTest.Some;
+import com.ijioio.test.model.PropertyEntityReferenceListFinalDeletePersistence;
+import com.ijioio.test.model.PropertyEntityReferenceListFinalDeletePersistenceIndex;
+
+public class PropertyEntityReferenceListFinalDeletePersistenceTest extends
+		BasePropertyCollectionDeletePersistenceTest<PropertyEntityReferenceListFinalDeletePersistenceIndex, List<EntityReference<? extends Some>>, EntityReference<? extends Some>> {
+
+	@Entity( //
+			name = PropertyEntityReferenceListFinalDeletePersistencePrototype.NAME, //
+			types = { //
+					@Type(name = "EntityReference<? extends Some>", type = Type.ENTITY_REFERENCE, parameters = @Parameter(name = Some.NAME, wildcard = true)), //
+					@Type(name = "List<EntityReference<? extends Some>>", type = Type.LIST, parameters = @Parameter(name = "EntityReference<? extends Some>")) //
+			}, //
+			properties = { //
+					@EntityProperty(name = "valueEntityReferenceList", type = "List<EntityReference<? extends Some>>") //
+			}, //
+			indexes = { //
+					@EntityIndex( //
+							name = PropertyEntityReferenceListFinalDeletePersistencePrototype.INDEX_NAME, //
+							properties = { //
+									@EntityIndexProperty(name = "valueEntityReferenceList", type = "List<EntityReference<? extends Some>>", attributes = Attribute.FINAL) //
+							} //
+					) //
+			} //
+	)
+	public static interface PropertyEntityReferenceListFinalDeletePersistencePrototype {
+
+		public static final String NAME = "com.ijioio.test.model.PropertyEntityReferenceListFinalDeletePersistence";
+
+		public static final String INDEX_NAME = "com.ijioio.test.model.PropertyEntityReferenceListFinalDeletePersistenceIndex";
+	}
+
+	@Override
+	protected String getSqlScriptFileName() throws Exception {
+		return "property-entity-reference-list-final-delete-persistence.sql";
+	}
+
+	@Override
+	protected boolean isFinal() {
+		return true;
+	}
+
+	@Override
+	protected Class<PropertyEntityReferenceListFinalDeletePersistenceIndex> getIndexClass() {
+		return PropertyEntityReferenceListFinalDeletePersistenceIndex.class;
+	}
+
+	@Override
+	protected List<PropertyEntityReferenceListFinalDeletePersistenceIndex> createIndexes() {
+
+		List<PropertyEntityReferenceListFinalDeletePersistenceIndex> indexes = new ArrayList<>();
+
+		int count = random.nextInt(INDEX_MAX_COUNT) + 1;
+
+		for (int i = 0; i < count; i++) {
+
+			PropertyEntityReferenceListFinalDeletePersistenceIndex index = new PropertyEntityReferenceListFinalDeletePersistenceIndex();
+
+			index.setId(String.format("property-entity-reference-list-final-delete-persistence-index-%s", i + 1));
+			index.setSource(EntityReference.of(
+					String.format("property-entity-reference-list-final-delete-persistence-%s", i + 1),
+					PropertyEntityReferenceListFinalDeletePersistence.class));
+
+			List<EntityReference<? extends Some>> value = new ArrayList<>();
+
+			for (int j = 0; j < VALUE_MAX_COUNT; j++) {
+				value.add(EntityReference.of(String.format("some-%s%s", i + 1, j + 1),
+						collectionTypes.get(i + 1).get(j)));
+			}
+
+			index.getValueEntityReferenceList().clear();
+			index.getValueEntityReferenceList().addAll(value);
+
+			indexes.add(index);
+		}
+
+		return indexes;
+	}
+
+	@Override
+	protected List<EntityReference<? extends Some>> createEmptyPropertyValue() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	protected List<EntityReference<? extends Some>> createAllNullPropertyValue() {
+
+		List<EntityReference<? extends Some>> value = new ArrayList<>();
+
+		for (int j = 0; j < VALUE_MAX_COUNT; j++) {
+			value.add(null);
+		}
+
+		return value;
+	}
+
+	@Override
+	protected List<EntityReference<? extends Some>> createAllSamePropertyValue(int i) {
+
+		List<EntityReference<? extends Some>> value = new ArrayList<>();
+
+		for (int j = 0; j < VALUE_MAX_COUNT; j++) {
+			value.add(EntityReference.of(String.format("some-%s", i + 1), types.get(i)));
+		}
+
+		return value;
+	}
+
+	@Override
+	protected Property<List<EntityReference<? extends Some>>> getProperty() {
+		return PropertyEntityReferenceListFinalDeletePersistenceIndex.Properties.valueEntityReferenceList;
+	}
+
+	@Override
+	protected List<EntityReference<? extends Some>> getPropertyValue(
+			PropertyEntityReferenceListFinalDeletePersistenceIndex index) {
+		return index.getValueEntityReferenceList();
+	}
+
+	@Override
+	protected void setPropertyValue(PropertyEntityReferenceListFinalDeletePersistenceIndex index,
+			List<EntityReference<? extends Some>> value) {
+
+		index.getValueEntityReferenceList().clear();
+		index.getValueEntityReferenceList().addAll(value);
+	}
+
+	@Override
+	protected int comparePropertyValue(List<EntityReference<? extends Some>> value1,
+			List<EntityReference<? extends Some>> value2) {
+		return compare(
+				Optional.ofNullable(value1)
+						.map(item -> item.stream().map(element -> getEntityReferenceId(element))
+								.collect(Collectors.toList()))
+						.orElse(null),
+				Optional.ofNullable(value2).map(item -> item.stream().map(element -> getEntityReferenceId(element))
+						.collect(Collectors.toList())).orElse(null));
+	}
+
+	@Override
+	protected int comparePropertyValueElement(EntityReference<? extends Some> element1,
+			EntityReference<? extends Some> element2) {
+		return compare(getEntityReferenceId(element1), getEntityReferenceId(element2));
+	}
+
+	@Override
+	protected void checkPropertyValue(List<EntityReference<? extends Some>> expectedValue,
+			List<EntityReference<? extends Some>> actualValue) {
+
+		Assertions.assertEquals(
+				Optional.ofNullable(expectedValue)
+						.map(item -> item.stream().map(element -> getEntityReferenceId(element))
+								.collect(Collectors.toList()))
+						.orElse(null),
+				Optional.ofNullable(actualValue).map(item -> item.stream().map(element -> getEntityReferenceId(element))
+						.collect(Collectors.toList())).orElse(null));
+		Assertions.assertEquals(
+				Optional.ofNullable(expectedValue)
+						.map(item -> item.stream().map(element -> getEntityReferenceTypeName(element))
+								.collect(Collectors.toList()))
+						.orElse(null),
+				Optional.ofNullable(actualValue).map(item -> item.stream()
+						.map(element -> getEntityReferenceTypeName(element)).collect(Collectors.toList()))
+						.orElse(null));
+	}
+
+	private String getEntityReferenceId(EntityReference<? extends Some> element) {
+		return Optional.ofNullable(element).map(item -> item.getId()).orElse(null);
+	}
+
+	private String getEntityReferenceTypeName(EntityReference<? extends Some> element) {
+		return Optional.ofNullable(element).map(item -> item.getType()).map(item -> item.getName()).orElse(null);
+	}
+}
---
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/persistence/index/property/PropertyStringListFinalDeletePersistenceTest.java
@@ -0,0 +1,153 @@
+package com.ijioio.aes.sandbox.test.persistence.index.property;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+
+import com.ijioio.aes.annotation.Attribute;
+import com.ijioio.aes.annotation.Entity;
+import com.ijioio.aes.annotation.EntityIndex;
+import com.ijioio.aes.annotation.EntityIndexProperty;
+import com.ijioio.aes.annotation.EntityProperty;
+import com.ijioio.aes.annotation.Parameter;
+import com.ijioio.aes.annotation.Type;
+import com.ijioio.aes.core.EntityReference;
+import com.ijioio.aes.core.Property;
+import com.ijioio.test.model.PropertyStringListFinalDeletePersistence;
+import com.ijioio.test.model.PropertyStringListFinalDeletePersistenceIndex;
+
+public class PropertyStringListFinalDeletePersistenceTest extends
+		BasePropertyCollectionDeletePersistenceTest<PropertyStringListFinalDeletePersistenceIndex, List<String>, String> {
+
+	@Entity( //
+			name = PropertyStringListDeletePersistencePrototype.NAME, //
+			types = { //
+					@Type(name = "List<String>", type = Type.LIST, parameters = @Parameter(name = Type.STRING)) //
+			}, //
+			properties = { //
+					@EntityProperty(name = "valueStringList", type = "List<String>") //
+			}, //
+			indexes = { //
+					@EntityIndex( //
+							name = PropertyStringListDeletePersistencePrototype.INDEX_NAME, //
+							properties = { //
+									@EntityIndexProperty(name = "valueStringList", type = "List<String>", attributes = Attribute.FINAL) //
+							} //
+					) //
+			} //
+	)
+	public static interface PropertyStringListDeletePersistencePrototype {
+
+		public static final String NAME = "com.ijioio.test.model.PropertyStringListFinalDeletePersistence";
+
+		public static final String INDEX_NAME = "com.ijioio.test.model.PropertyStringListFinalDeletePersistenceIndex";
+	}
+
+	@Override
+	protected String getSqlScriptFileName() throws Exception {
+		return "property-string-list-final-delete-persistence.sql";
+	}
+
+	@Override
+	protected boolean isFinal() {
+		return true;
+	}
+
+	@Override
+	protected Class<PropertyStringListFinalDeletePersistenceIndex> getIndexClass() {
+		return PropertyStringListFinalDeletePersistenceIndex.class;
+	}
+
+	@Override
+	protected List<PropertyStringListFinalDeletePersistenceIndex> createIndexes() {
+
+		List<PropertyStringListFinalDeletePersistenceIndex> indexes = new ArrayList<>();
+
+		int count = random.nextInt(INDEX_MAX_COUNT) + 1;
+
+		for (int i = 0; i < count; i++) {
+
+			PropertyStringListFinalDeletePersistenceIndex index = new PropertyStringListFinalDeletePersistenceIndex();
+
+			index.setId(String.format("property-string-list-final-delete-persistence-index-%s", i + 1));
+			index.setSource(EntityReference.of(String.format("property-string-list-final-delete-persistence-%s", i + 1),
+					PropertyStringListFinalDeletePersistence.class));
+
+			List<String> value = new ArrayList<>();
+
+			for (int j = 0; j < VALUE_MAX_COUNT; j++) {
+				value.add(String.format("value%s%s", i + 1, j + 1));
+			}
+
+			index.getValueStringList().clear();
+			index.getValueStringList().addAll(value);
+
+			indexes.add(index);
+		}
+
+		return indexes;
+	}
+
+	@Override
+	protected List<String> createEmptyPropertyValue() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	protected List<String> createAllNullPropertyValue() {
+
+		List<String> value = new ArrayList<>();
+
+		for (int j = 0; j < VALUE_MAX_COUNT; j++) {
+			value.add(null);
+		}
+
+		return value;
+	}
+
+	@Override
+	protected List<String> createAllSamePropertyValue(int i) {
+
+		List<String> value = new ArrayList<>();
+
+		for (int j = 0; j < VALUE_MAX_COUNT; j++) {
+			value.add(String.format("value%s", i + 1));
+		}
+
+		return value;
+	}
+
+	@Override
+	protected Property<List<String>> getProperty() {
+		return PropertyStringListFinalDeletePersistenceIndex.Properties.valueStringList;
+	}
+
+	@Override
+	protected List<String> getPropertyValue(PropertyStringListFinalDeletePersistenceIndex index) {
+		return index.getValueStringList();
+	}
+
+	@Override
+	protected void setPropertyValue(PropertyStringListFinalDeletePersistenceIndex index, List<String> value) {
+
+		index.getValueStringList().clear();
+		index.getValueStringList().addAll(value);
+	}
+
+	@Override
+	protected int comparePropertyValue(List<String> value1, List<String> value2) {
+		return compare(value1, value2);
+	}
+
+	@Override
+	protected int comparePropertyValueElement(String element1, String element2) {
+		return compare(element1, element2);
+	}
+
+	@Override
+	protected void checkPropertyValue(List<String> expectedValue, List<String> actualValue) {
+		Assertions.assertEquals(expectedValue, actualValue);
+	}
+}
