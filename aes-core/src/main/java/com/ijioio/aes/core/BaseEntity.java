8a8ebb0445ab51464ab634ac321269f34429a91d
abeleshev <abeleshev@gmail.com>
2023-10-25 22:55:11 +0900

    Update entity interface to implement introspectable (#118)

--- aes-core/src/main/java/com/ijioio/aes/core/BaseEntity.java
+++ aes-core/src/main/java/com/ijioio/aes/core/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.ijioio.aes.core;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.ijioio.aes.core.serialization.SerializationContext;
@@ -13,6 +16,45 @@ import com.ijioio.aes.core.serialization.SerializationWriter;
  * Base class for all entities.
  */
 public abstract class BaseEntity extends BaseIdentity implements Entity {
+
+	public static class Properties {
+
+		public static final Property<String> id = Property.of("id", new TypeReference<String>() {
+		});
+
+		private static final List<Property<?>> values = new ArrayList<>();
+
+		static {
+
+			values.add(id);
+		}
+	}
+
+	@Override
+	public Collection<Property<?>> getProperties() {
+		return Properties.values;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T read(Property<T> property) throws IntrospectionException {
+
+		if (Properties.id.equals(property)) {
+			return (T) getId();
+		} else {
+			throw new IntrospectionException(String.format("property %s is not supported", property));
+		}
+	}
+
+	@Override
+	public <T> void write(Property<T> property, T value) throws IntrospectionException {
+
+		if (Properties.id.equals(property)) {
+			setId((String) value);
+		} else {
+			throw new IntrospectionException(String.format("property %s is not supported", property));
+		}
+	}
 
 	@Override
 	public void write(SerializationContext context, SerializationHandler handler) throws SerializationException {
--- aes-core/src/main/java/com/ijioio/aes/core/Entity.java
+++ aes-core/src/main/java/com/ijioio/aes/core/Entity.java
@@ -1,8 +1,11 @@
 package com.ijioio.aes.core;
 
+import java.util.Collection;
+
 /**
  * Interface defining entity. Entity is a simple object containing properties.
  */
-public interface Entity extends Identity, XSerializable {
+public interface Entity extends Identity, Introspectable, XSerializable {
 
+	public Collection<Property<?>> getProperties();
 }
