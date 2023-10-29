778615df0ac1a1da5005a604ddf6fdf09ddd4b4e
abeleshev <abeleshev@gmail.com>
2023-10-30 01:19:32 +0900

    Update base serialization test (#121)

--- aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/BaseSerializationTest.java
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/BaseSerializationTest.java
@@ -1,14 +1,7 @@
 package com.ijioio.aes.sandbox.test.serialization;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
+import com.ijioio.aes.sandbox.test.BaseTest;
 
-public class BaseSerializationTest {
+public class BaseSerializationTest extends BaseTest {
 
-	protected String readString(Path path) throws IOException {
-		return Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.joining("\n"));
-	}
 }
