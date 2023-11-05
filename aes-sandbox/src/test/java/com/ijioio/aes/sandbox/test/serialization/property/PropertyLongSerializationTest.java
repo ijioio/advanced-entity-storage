a115f8e6d78f6013f3e29dc5265a8f60787edaa2
abeleshev <abeleshev@gmail.com>
2023-11-05 14:32:13 +0900

    Add long property serialization test (#121)

---
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/property/PropertyLongSerializationTest.java
@@ -0,0 +1,54 @@
+package com.ijioio.aes.sandbox.test.serialization.property;
+
+import org.junit.jupiter.api.Assertions;
+
+import com.ijioio.aes.annotation.Entity;
+import com.ijioio.aes.annotation.EntityProperty;
+import com.ijioio.aes.annotation.Type;
+import com.ijioio.test.model.PropertyByteSerialization;
+import com.ijioio.test.model.PropertyLongSerialization;
+
+public class PropertyLongSerializationTest extends BasePropertySerializationTest<PropertyLongSerialization, Long> {
+
+	@Entity( //
+			name = PropertyLongSerializationPrototype.NAME, //
+			properties = { //
+					@EntityProperty(name = "valueLong", type = Type.LONG) //
+			} //
+	)
+	public static interface PropertyLongSerializationPrototype {
+
+		public static final String NAME = "com.ijioio.test.model.PropertyLongSerialization";
+	}
+
+	@Override
+	protected String getXmlFileName() throws Exception {
+		return "property-long-serialization.xml";
+	}
+
+	@Override
+	protected Class<PropertyLongSerialization> getEntityClass() {
+		return PropertyLongSerialization.class;
+	}
+
+	@Override
+	protected PropertyLongSerialization createEntity() {
+
+		PropertyLongSerialization entity = new PropertyLongSerialization();
+
+		entity.setId("property-long-serialization");
+		entity.setValueLong(1);
+
+		return entity;
+	}
+
+	@Override
+	protected Long getPropertyValue(PropertyLongSerialization entity) {
+		return entity.getValueLong();
+	}
+
+	@Override
+	protected void checkPropertyValue(Long expectedValue, Long actualValue) {
+		Assertions.assertEquals(expectedValue, actualValue);
+	}
+}
