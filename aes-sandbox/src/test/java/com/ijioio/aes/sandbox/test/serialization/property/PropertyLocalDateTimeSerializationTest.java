75356a5a7cd5e4769e0cf637eacd6e15e6b5c332
abeleshev <abeleshev@gmail.com>
2023-11-06 14:56:16 +0900

    Add local date time property serialization test (#121)

--- aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/property/PropertyLocalDateTimeSerializationTest.java
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/property/PropertyLocalDateTimeSerializationTest.java
@@ -1,23 +1,17 @@
 package com.ijioio.aes.sandbox.test.serialization.property;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.Month;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import com.ijioio.aes.annotation.Entity;
 import com.ijioio.aes.annotation.EntityProperty;
 import com.ijioio.aes.annotation.Type;
-import com.ijioio.aes.core.serialization.xml.XmlSerializationHandler;
-import com.ijioio.aes.core.serialization.xml.XmlUtil;
-import com.ijioio.aes.sandbox.test.serialization.BaseSerializationTest;
 import com.ijioio.test.model.PropertyLocalDateTimeSerialization;
 
-public class PropertyLocalDateTimeSerializationTest extends BaseSerializationTest {
+public class PropertyLocalDateTimeSerializationTest
+		extends BasePropertySerializationTest<PropertyLocalDateTimeSerialization, LocalDateTime> {
 
 	@Entity( //
 			name = PropertyLocalDateTimeSerializationPrototype.NAME, //
@@ -30,42 +24,34 @@ public class PropertyLocalDateTimeSerializationTest extends BaseSerializationTes
 		public static final String NAME = "com.ijioio.test.model.PropertyLocalDateTimeSerialization";
 	}
 
-	private Path path;
-
-	private PropertyLocalDateTimeSerialization model;
-
-	@BeforeEach
-	public void before() throws Exception {
-
-		path = Paths.get(getClass().getClassLoader().getResource("property-local-date-time-serialization.xml").toURI());
-
-		model = new PropertyLocalDateTimeSerialization();
-
-		model.setId("property-local-date-time-serialization");
-		model.setValueLocalDateTime(LocalDateTime.of(2022, Month.AUGUST, 22, 14, 25, 40, 123456789));
+	@Override
+	protected String getXmlFileName() throws Exception {
+		return "property-local-date-time-serialization.xml";
 	}
 
-	@Test
-	public void testWrite() throws Exception {
-
-		XmlSerializationHandler handler = new XmlSerializationHandler();
-
-		String actual = XmlUtil.write(handler, model);
-		String expected = readString(path);
-
-		Assertions.assertEquals(expected, actual);
+	@Override
+	protected Class<PropertyLocalDateTimeSerialization> getEntityClass() {
+		return PropertyLocalDateTimeSerialization.class;
 	}
 
-	@Test
-	public void testRead() throws Exception {
+	@Override
+	protected PropertyLocalDateTimeSerialization createEntity() {
 
-		XmlSerializationHandler handler = new XmlSerializationHandler();
+		PropertyLocalDateTimeSerialization entity = new PropertyLocalDateTimeSerialization();
 
-		PropertyLocalDateTimeSerialization actual = XmlUtil.read(handler, PropertyLocalDateTimeSerialization.class,
-				readString(path));
-		PropertyLocalDateTimeSerialization expected = model;
+		entity.setId("property-local-date-time-serialization");
+		entity.setValueLocalDateTime(LocalDateTime.of(2022, Month.AUGUST, 22, 14, 25, 40, 123456789));
 
-		Assertions.assertEquals(expected.getId(), actual.getId());
-		Assertions.assertEquals(expected.getValueLocalDateTime(), actual.getValueLocalDateTime());
+		return entity;
+	}
+
+	@Override
+	protected LocalDateTime getPropertyValue(PropertyLocalDateTimeSerialization entity) {
+		return entity.getValueLocalDateTime();
+	}
+
+	@Override
+	protected void checkPropertyValue(LocalDateTime expectedValue, LocalDateTime actualValue) {
+		Assertions.assertEquals(expectedValue, actualValue);
 	}
 }
