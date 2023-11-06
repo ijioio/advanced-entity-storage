fbc371401361dcd54a64f63fee2af9663a56f3c2
abeleshev <abeleshev@gmail.com>
2023-11-06 14:45:04 +0900

    Add local date property serialization test (#121)

--- aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/property/PropertyLocalDateSerializationTest.java
+++ aes-sandbox/src/test/java/com/ijioio/aes/sandbox/test/serialization/property/PropertyLocalDateSerializationTest.java
@@ -1,23 +1,17 @@
 package com.ijioio.aes.sandbox.test.serialization.property;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import com.ijioio.aes.annotation.Entity;
 import com.ijioio.aes.annotation.EntityProperty;
 import com.ijioio.aes.annotation.Type;
-import com.ijioio.aes.core.serialization.xml.XmlSerializationHandler;
-import com.ijioio.aes.core.serialization.xml.XmlUtil;
-import com.ijioio.aes.sandbox.test.serialization.BaseSerializationTest;
 import com.ijioio.test.model.PropertyLocalDateSerialization;
 
-public class PropertyLocalDateSerializationTest extends BaseSerializationTest {
+public class PropertyLocalDateSerializationTest
+		extends BasePropertySerializationTest<PropertyLocalDateSerialization, LocalDate> {
 
 	@Entity( //
 			name = PropertyLocalDateSerializationPrototype.NAME, //
@@ -30,42 +24,34 @@ public class PropertyLocalDateSerializationTest extends BaseSerializationTest {
 		public static final String NAME = "com.ijioio.test.model.PropertyLocalDateSerialization";
 	}
 
-	private Path path;
-
-	private PropertyLocalDateSerialization model;
-
-	@BeforeEach
-	public void before() throws Exception {
-
-		path = Paths.get(getClass().getClassLoader().getResource("property-local-date-serialization.xml").toURI());
-
-		model = new PropertyLocalDateSerialization();
-
-		model.setId("property-local-date-serialization");
-		model.setValueLocalDate(LocalDate.of(2022, Month.AUGUST, 22));
+	@Override
+	protected String getXmlFileName() throws Exception {
+		return "property-local-date-serialization.xml";
 	}
 
-	@Test
-	public void testWrite() throws Exception {
-
-		XmlSerializationHandler handler = new XmlSerializationHandler();
-
-		String actual = XmlUtil.write(handler, model);
-		String expected = readString(path);
-
-		Assertions.assertEquals(expected, actual);
+	@Override
+	protected Class<PropertyLocalDateSerialization> getEntityClass() {
+		return PropertyLocalDateSerialization.class;
 	}
 
-	@Test
-	public void testRead() throws Exception {
+	@Override
+	protected PropertyLocalDateSerialization createEntity() {
 
-		XmlSerializationHandler handler = new XmlSerializationHandler();
+		PropertyLocalDateSerialization entity = new PropertyLocalDateSerialization();
 
-		PropertyLocalDateSerialization actual = XmlUtil.read(handler, PropertyLocalDateSerialization.class,
-				readString(path));
-		PropertyLocalDateSerialization expected = model;
+		entity.setId("property-local-date-serialization");
+		entity.setValueLocalDate(LocalDate.of(2022, Month.AUGUST, 22));
 
-		Assertions.assertEquals(expected.getId(), actual.getId());
-		Assertions.assertEquals(expected.getValueLocalDate(), actual.getValueLocalDate());
+		return entity;
+	}
+
+	@Override
+	protected LocalDate getPropertyValue(PropertyLocalDateSerialization entity) {
+		return entity.getValueLocalDate();
+	}
+
+	@Override
+	protected void checkPropertyValue(LocalDate expectedValue, LocalDate actualValue) {
+		Assertions.assertEquals(expectedValue, actualValue);
 	}
 }
