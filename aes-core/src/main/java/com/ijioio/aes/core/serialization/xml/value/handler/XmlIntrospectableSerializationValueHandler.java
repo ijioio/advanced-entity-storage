15756015489b6b317645b13bda0d175e4ed01969
abeleshev <abeleshev@gmail.com>
2023-11-13 01:07:23 +0900

    Add null check on introspectable serialization handler read (#121)

--- aes-core/src/main/java/com/ijioio/aes/core/serialization/xml/value/handler/XmlIntrospectableSerializationValueHandler.java
+++ aes-core/src/main/java/com/ijioio/aes/core/serialization/xml/value/handler/XmlIntrospectableSerializationValueHandler.java
@@ -96,7 +96,7 @@ public class XmlIntrospectableSerializationValueHandler extends BaseXmlSerializa
 							: property.getType().getRawType();
 
 					// TODO: take into account final field, i.e. use null for non final field!
-					Object propertyValue = value.read(property);
+					Object propertyValue = value != null ? value.read(property) : null;
 
 					introspectable.write((Property) property,
 							((XmlSerializationValueHandler) handler.getValueHandler(propertyType)).read(context,
