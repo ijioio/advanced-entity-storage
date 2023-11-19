12d9866490287b5194182495c354c728fd613619
abeleshev <abeleshev@gmail.com>
2023-11-19 22:38:44 +0900

    Update serialization manager to use entity (#121)

--- aes-core/src/main/java/com/ijioio/aes/core/serialization/xml/XmlSerializationManager.java
+++ aes-core/src/main/java/com/ijioio/aes/core/serialization/xml/XmlSerializationManager.java
@@ -3,7 +3,7 @@ package com.ijioio.aes.core.serialization.xml;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
-import com.ijioio.aes.core.XSerializable;
+import com.ijioio.aes.core.Entity;
 import com.ijioio.aes.core.serialization.SerializationException;
 import com.ijioio.aes.core.serialization.SerializationManager;
 
@@ -20,17 +20,17 @@ public class XmlSerializationManager implements SerializationManager {
 	}
 
 	@Override
-	public <X extends XSerializable> X read(Class<X> type, byte[] data) throws SerializationException {
+	public <E extends Entity> E read(Class<E> type, byte[] data) throws SerializationException {
 
 		ByteArrayInputStream bais = new ByteArrayInputStream(data);
 
-		X object = XmlUtil.read(handler, type, bais);
+		E object = XmlUtil.read(handler, type, bais);
 
 		return object;
 	}
 
 	@Override
-	public <X extends XSerializable> byte[] write(X object) throws SerializationException {
+	public <E extends Entity> byte[] write(E object) throws SerializationException {
 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
