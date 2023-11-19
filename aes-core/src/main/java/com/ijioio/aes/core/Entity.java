f108eb833d0656f57dafd1609704019552e45a71
abeleshev <abeleshev@gmail.com>
2023-11-19 22:32:24 +0900

    Update entity to remove xserializable interface (#121)

--- aes-core/src/main/java/com/ijioio/aes/core/Entity.java
+++ aes-core/src/main/java/com/ijioio/aes/core/Entity.java
@@ -3,6 +3,6 @@ package com.ijioio.aes.core;
 /**
  * Interface defining entity. Entity is a simple object containing properties.
  */
-public interface Entity extends Identity, Introspectable, XSerializable {
+public interface Entity extends Identity, Introspectable {
 
 }
