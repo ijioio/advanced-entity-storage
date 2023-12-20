d762b625cfa57017719bf47d762e1e8d44647222
abeleshev <abeleshev@gmail.com>
2023-12-20 22:25:57 +0900

    Update persistence handler interface to use entity data (#128)

--- aes-core/src/main/java/com/ijioio/aes/core/persistence/PersistenceHandler.java
+++ aes-core/src/main/java/com/ijioio/aes/core/persistence/PersistenceHandler.java
@@ -2,12 +2,25 @@ package com.ijioio.aes.core.persistence;
 
 import java.util.List;
 
+import com.ijioio.aes.core.Entity;
 import com.ijioio.aes.core.EntityIndex;
+import com.ijioio.aes.core.EntityReference;
 import com.ijioio.aes.core.SearchQuery;
+import com.ijioio.aes.core.entity.storage.EntityData;
 
 public interface PersistenceHandler {
 
 	public PersistenceTransaction createTransaction() throws PersistenceException;
+
+	public PersistenceTransaction obtainTransaction() throws PersistenceException;
+
+	public <E extends Entity> void create(EntityData<E> data) throws PersistenceException;
+
+	public <E extends Entity> void update(EntityData<E> data) throws PersistenceException;
+
+	public <E extends Entity> void delete(EntityData<E> data) throws PersistenceException;
+
+	public <E extends Entity> EntityData<E> load(EntityReference<E> reference) throws PersistenceException;
 
 	public void create(EntityIndex<?> index) throws PersistenceException;
 
